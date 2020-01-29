e0daf0f2d19766715596925f0b52c3fadd6a0fa6
eugenemonnier <eugene.monnier@gmail.com>
2020-01-28 22:55:20 -0800

    Movie posters are dynamically rendering on main page!

--- server/server.js
+++ server/server.js
@@ -1,7 +1,5 @@
 'use strict';
 
-
-
 // Application Dependencies
 const express = require('express');
 const superagent = require('superagent');
@@ -30,44 +28,51 @@ client.on('error', err => console.error('error'));
 
 
 // routes
-app.get('/', mainPageLoad);
-
-
+app.get('/', mainPage);
 
 // function for the routes to be view in localhost
-function mainPageLoad (request, response) {
-  let query = `https://api.themoviedb.org/3/movie/now_playing?api_key=${process.env.TMDB_API_KEY}&language=en-US&page=1&region=US`;
-  superagent.get(query)
-    .then(results => {
-      let movieArray = results.body.results;
-      const movies = movieArray.map(movie => {
-        let newMovie = new Movie(movie);
-        addMovie(newMovie);
-      });
-      response.status(200).render('index');
-    })
-    .catch(error => errorHandler(error, request,response));
+// calls getMovies function then renders index.ejs
+function mainPage (request, response) {
+  getMovies(request,response)
+    .then(movies => response.status(200).render('index.ejs', {posters: movies,}));
 }
+
+// queries TMDB for currently in theater movies, calls constructor array to create movie objects and addMovie function to store them
+function getMovies (request, response) {
+  try {
+    let query = `https://api.themoviedb.org/3/movie/now_playing?api_key=${process.env.TMDB_API_KEY}&language=en-US&page=1&region=US`;
+    return superagent.get(query)
+      .then(results => {
+        let movieArray = results.body.results;
+        const movies = movieArray.map(movie => addMovie(new Movie(movie)));
+        return movies;
+      });
+  }
+  catch(error) {
+    errorHandler(error, request, response);
+  }
+}
+
+// likely no longer needed
 function grabImg(movie) {
   let query = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.TMDB_API_KEY}&language=en-US&query=${movie.title}&page=1&include_adult=false`;
-  // console.log('TMDB query: ', query);
   return superagent.get(query)
     .then(results => {
       updateImg(results);
-
-      // console.log(`https://image.tmdb.org/t/p/w300_and_h450_bestv2${results.body.results[0].poster_path}`);
     });
 }
 
+// likely no longer needed
 function updateImg(results) {
   let sql = 'UPDATE movies SET img_url = $1 WHERE LOWER(title) = LOWER($2);';
   let moviePoster = '';
   results.body.results[0].poster_path === undefined ? moviePoster = `https://via.placeholder.com/300x450.JPEG?text=${(results.body.results[0].title).split(' ').join('+')}`
-  : moviePoster = `https://image.tmdb.org/t/p/w300_and_h450_bestv2${results.body.results[0].poster_path}`;
-  let safeWords = [moviePoster, results.body.results[0].title]
+    : moviePoster = `https://image.tmdb.org/t/p/w300_and_h450_bestv2${results.body.results[0].poster_path}`;
+  let safeWords = [moviePoster, results.body.results[0].title];
   client.query(sql,safeWords);
 }
 
+// will likely need. Get's today's date and returns it formatted as YYYY-(M)M-DD
 function todayDate() {
   let date = new Date();
   let dateValues = [
@@ -78,11 +83,16 @@ function todayDate() {
   let today = dateValues.join('-');
   return today;
 }
+
+// Inserts values from each movie obj into movies table
 function addMovie(movie){
   let sql = 'INSERT INTO movies (title, movie_description, img_url) VALUES ($1, $2, $3);';
   let safeWords = [movie.title, movie.movieDescription, movie.imgURL];
   client.query(sql,safeWords);
+  return movie;
 }
+
+// Constructor to create movie object
 function Movie(data) {
   this.title = data.title;
   this.movieDescription = data.overview;
@@ -99,9 +109,6 @@ function errorHandler(err, request, response) {
 function developerErrorHandler(request, response) {
   response.status(404).send('sorry this request is not available yet');
 }
-
-
-
 
 app.use('*', developerErrorHandler);
 
